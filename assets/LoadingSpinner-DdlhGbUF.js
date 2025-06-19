653e5fe09ae6b2f928784d7f092225fa9f2a2dde
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-19 01:48:13 -0300

    Updates

--- assets/LoadingSpinner-DdlhGbUF.js
+++ assets/LoadingSpinner-DdlhGbUF.js
@@ -1,4 +1,4 @@
-import{d as a,j as e}from"./index-7CHcPrxa.js";/**
+import{d as a,j as e}from"./index-v1kmWTwr.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
