aa9065c66ac72245e5848bb0c9ee48365313d31a
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-19 03:03:36 -0300

    Updates

--- assets/LoadingSpinner-DhxP1Db9.js
+++ assets/LoadingSpinner-DhxP1Db9.js
@@ -1,4 +1,4 @@
-import{e as a,j as e}from"./index-QdsocXSw.js";/**
+import{e as a,j as e}from"./index-BlTBJpj7.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
