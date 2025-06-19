a254fcdb3cd1738f84924191843a46bbc0da9016
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-18 22:22:19 -0300

    Updates

--- assets/arrow-left-dPGlM-bL.js
+++ assets/arrow-left-dPGlM-bL.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-BMqE3RHN.js";/**
+import{d as e}from"./index-ft7LEQCh.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
