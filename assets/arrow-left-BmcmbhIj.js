51876d2ad18bc3d3b44e89b73cd120565c9390cf
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-18 22:16:30 -0300

    Updates

--- assets/arrow-left-BmcmbhIj.js
+++ assets/arrow-left-BmcmbhIj.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-BrDTSbCB.js";/**
+import{d as e}from"./index-BMqE3RHN.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
