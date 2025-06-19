e2487e7f51ff72d74e63723a5b5b828c51154219
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-18 21:17:18 -0300

    Updates

--- assets/arrow-left-V6eZ0WMn.js
+++ assets/arrow-left-V6eZ0WMn.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-BpjEaMRJ.js";/**
+import{d as e}from"./index-BrDTSbCB.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
