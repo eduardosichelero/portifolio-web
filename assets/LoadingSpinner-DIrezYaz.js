f7fb1d8f56c7283ab2528949366bf4c8030879a9
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-18 23:26:47 -0300

    Updates

--- assets/LoadingSpinner-DIrezYaz.js
+++ assets/LoadingSpinner-DIrezYaz.js
@@ -1,4 +1,4 @@
-import{d as a,j as e}from"./index-t8WTNt6L.js";/**
+import{d as a,j as e}from"./index-tYXpE4m4.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
