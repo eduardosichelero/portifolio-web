b16eae10b09f0f4ff79aec86584135ba914c557e
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-18 22:30:50 -0300

    Updates

--- assets/arrow-left-5HuGgOGO.js
+++ assets/arrow-left-5HuGgOGO.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-ft7LEQCh.js";/**
+import{d as e}from"./index-4hGupiRI.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
