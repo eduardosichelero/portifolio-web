3a35bfba82a5a31bcec1268c35869c754b20008c
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-19 03:19:21 -0300

    Updates

--- assets/LoadingSpinner-3-BHDiUd.js
+++ assets/LoadingSpinner-3-BHDiUd.js
@@ -1,4 +1,4 @@
-import{e as a,j as e}from"./index-1f1y1Xem.js";/**
+import{e as a,j as e}from"./index-BJ1cd8SF.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
