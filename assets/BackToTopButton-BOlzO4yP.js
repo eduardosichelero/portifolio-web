6bcc4a7bc166fb160d8be14c2ea013c9e38782f6
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-20 19:09:33 -0300

    Updates

--- assets/BackToTopButton-BOlzO4yP.js
+++ assets/BackToTopButton-BOlzO4yP.js
@@ -1,4 +1,4 @@
-import{c as i,r as t,j as o}from"./index-Ck4ca5H0.js";/**
+import{c as i,r as t,j as o}from"./index-C3hPd_4k.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
