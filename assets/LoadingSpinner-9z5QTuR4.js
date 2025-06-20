d38d52a6444ed4f5af73ed475a670d3194676266
eduardosichelero <eduardoddssichelero@gmail.com>
2025-06-20 12:49:10 -0300

    Updates

--- assets/LoadingSpinner-9z5QTuR4.js
+++ assets/LoadingSpinner-9z5QTuR4.js
@@ -1,4 +1,4 @@
-import{e as a,j as e}from"./index-BJ1cd8SF.js";/**
+import{e as a,j as e}from"./index-RKfKg_Wq.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
