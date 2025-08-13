a7f588cd4f64b3a3e9b8404991ea190f4aa2e361
eduardosichelero <eduardoddssichelero@gmail.com>
2025-08-13 12:09:36 -0300

    Updates

--- assets/BackToTopButton-C9EE4YMf.js
+++ assets/BackToTopButton-C9EE4YMf.js
@@ -1,4 +1,4 @@
-import{c as i,r as t,j as o}from"./index-CN4Wp6P_.js";/**
+import{c as i,r as t,j as o}from"./index-Ck4ca5H0.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/LoadingSpinner-DC1kSuG1.js
+++ assets/LoadingSpinner-DC1kSuG1.js
@@ -1,4 +1,4 @@
-import{c as a,j as e}from"./index-CN4Wp6P_.js";/**
+import{c as a,j as e}from"./index-Ck4ca5H0.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
